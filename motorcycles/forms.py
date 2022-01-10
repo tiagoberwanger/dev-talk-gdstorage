45be41f19c58ee8b1b6902bfb32a82ec4cd55e91
Tiago Berwanger <berwangertiago@gmail.com>
2022-01-09 23:08:55 -0300

    refactoring

--- motorcycles/forms.py
+++ motorcycles/forms.py
@@ -6,6 +6,10 @@ class MotorcycleForm(ModelForm):
     class Meta:
         model = Motorcycle
         fields = ['motorcycle_name', 'motorcycle_brand', 'motorcycle_image']
-
+        labels = {
+            'motorcycle_name': ('Digite o nome da sua motocicleta'),
+            'motorcycle_brand': ('Digite a marca da sua motocicleta'),
+            'motorcycle_image': ('Escolha a imagem'),
+        }
 # Creating a form to add an article.
 form = MotorcycleForm()
--- motorcycles/models.py
+++ motorcycles/models.py
@@ -9,15 +9,7 @@ class Motorcycle(models.Model):
     id = models.AutoField(primary_key=True)
     motorcycle_name = models.CharField(max_length=200)
     motorcycle_brand = models.CharField(max_length=200)
-    motorcycle_image = models.ImageField(upload_to='images', storage=gd_storage)
+    motorcycle_image = models.ImageField(upload_to='images', storage=gd_storage, blank=True, default=None)
 
     def __str__(self):
         return self.motorcycle_name
-
-    # @property
-    # def get_image_url(self):
-    #     if self.motorcycle_image and hasattr(self.motorcycle_image, 'url'):
-    #         return self.motorcycle_image.url
-    #     else:
-    #         return "/static/images/no-image.png"
-
